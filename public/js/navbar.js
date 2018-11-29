f3e96bf4794a5f62dacbd30b3e734fb2358e8cac
Andrew Simpson <andrew.c.simpson@hotmail.co.uk>
2018-11-29 23:44:36 +0000

    more detal page

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -42,19 +42,19 @@
  
 
 
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-64px";
-      document.getElementsByTagName("header")[0].classList.remove("mobileMenuOpen");
-      document.getElementById("hamburger").classList.remove("open");
-      document.getElementById("hamburger").removeEventListener("click", function() {toggleMobileMenu(false);}, false);
-      document.getElementById("hamburger").addEventListener("click", function() {toggleMobileMenu(true);}, false);
-    }
-    prevScrollpos = currentScrollPos;
-  }
+  // var prevScrollpos = window.pageYOffset;
+  // window.onscroll = function() {
+  // var currentScrollPos = window.pageYOffset;
+  //   if (prevScrollpos > currentScrollPos) {
+  //     document.getElementById("navbar").style.top = "0";
+  //   } else {
+  //     document.getElementById("navbar").style.top = "-64px";
+  //     document.getElementsByTagName("header")[0].classList.remove("mobileMenuOpen");
+  //     document.getElementById("hamburger").classList.remove("open");
+  //     document.getElementById("hamburger").removeEventListener("click", function() {toggleMobileMenu(false);}, false);
+  //     document.getElementById("hamburger").addEventListener("click", function() {toggleMobileMenu(true);}, false);
+  //   }
+  //   prevScrollpos = currentScrollPos;
+  // };
 
   
